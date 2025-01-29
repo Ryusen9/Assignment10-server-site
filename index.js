586ca4c454d951c32a03fc63172e7947932aafd8
Ryusen9 <asratul02@gmail.com>
2025-01-29 11:38:43 +0600

    MongoDB integration

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const express = require('express')
 const cors = require('cors')
-
+require("dotenv").config();
 const app = express()
 const port = process.env.PORT || 3000;
 
 //middleware
 app.use(express.json());
 app.use(cors());
+
+//mongodb
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${user}:${password}@clusterryusen.ptw8o.mongodb.net/?retryWrites=true&w=majority&appName=ClusterRyusen`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send("Server is OKAY to go!")
